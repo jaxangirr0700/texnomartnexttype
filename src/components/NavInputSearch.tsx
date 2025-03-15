091b39cc2887564bacde7a4a00dd76c45aae6171
jaxangir <jaxangirr0700@gmail.com>
2025-03-15 05:27:03 +0500

    finished

--- src/components/NavInputSearch.tsx
+++ src/components/NavInputSearch.tsx
@@ -113,62 +113,69 @@ function NavInputSearch() {
     <div className="w-full flex flex-col items-center  relative font-bold">
       <SearchInput input={input} setInput={setInput} />
 
-      <div className="flex flex-col gap-8 absolute top-13 bg-slate-50 w-full rounded-b-2xl px-5 py-3 z-10">
-        <div className=" flex flex-wrap gap-2">
-          {products?.models?.map((c) => {
-            return (
-              <div key={c.id} className="flex gap-5  ">
-                <Link
-                  href={"/"}
-                  className=" rounded-xl border border-slate-300 px-2 py-1 bg-slate-100 hover:scale-105 hover:bg-slate-300 transition-all 0.7s"
-                >
-                  {c.title}
-                </Link>
-              </div>
-            );
-          })}
-        </div>{" "}
-        <div className=" flex flex-col">
-          {products?.categories?.map((c) => {
-            return (
-              <div
-                key={c.id}
-                className="flex gap-5 hover:bg-slate-300 transition-all 0.7s"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M5.25 9.75H9.75V5.25H5.25V9.75ZM3.75 5.24971C3.75 4.42144 4.41557 3.75 5.24971 3.75H9.75029C10.5786 3.75 11.25 4.41557 11.25 5.24971V9.75029C11.25 10.5786 10.5844 11.25 9.75029 11.25H5.24971C4.42144 11.25 3.75 10.5844 3.75 9.75029V5.24971ZM5.25 18.75H9.75V14.25H5.25V18.75ZM3.75 14.2497C3.75 13.4214 4.41557 12.75 5.24971 12.75H9.75029C10.5786 12.75 11.25 13.4156 11.25 14.2497V18.7503C11.25 19.5786 10.5844 20.25 9.75029 20.25H5.24971C4.42144 20.25 3.75 19.5844 3.75 18.7503V14.2497ZM14.25 18.75H18.75V14.25H14.25V18.75ZM12.75 14.2497C12.75 13.4214 13.4156 12.75 14.2497 12.75H18.7503C19.5786 12.75 20.25 13.4156 20.25 14.2497V18.7503C20.25 19.5786 19.5844 20.25 18.7503 20.25H14.2497C13.4214 20.25 12.75 19.5844 12.75 18.7503V14.2497ZM14.25 9.75H18.75V5.25H14.25V9.75ZM12.75 5.24971C12.75 4.42144 13.4156 3.75 14.2497 3.75H18.7503C19.5786 3.75 20.25 4.41557 20.25 5.24971V9.75029C20.25 10.5786 19.5844 11.25 18.7503 11.25H14.2497C13.4214 11.25 12.75 10.5844 12.75 9.75029V5.24971Z"
-                    fill="#BABAC0"
-                  ></path>
-                </svg>
+      {input.length > 0 ? (
+        <>
+          {" "}
+          <div className="flex flex-col gap-8 absolute top-13 bg-slate-50 w-full rounded-b-2xl px-5 py-3 z-10">
+            <div className=" flex flex-wrap gap-2">
+              {products?.models?.map((c) => {
+                return (
+                  <div key={c.id} className="flex gap-5  ">
+                    <Link
+                      href={"/"}
+                      className=" rounded-xl border border-slate-300 px-2 py-1 bg-slate-100 hover:scale-105 hover:bg-slate-300 transition-all 0.7s"
+                    >
+                      {c.title}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>{" "}
+            <div className=" flex flex-col">
+              {products?.categories?.map((c) => {
+                return (
+                  <div
+                    key={c.id}
+                    className="flex gap-5 hover:bg-slate-300 transition-all 0.7s"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M5.25 9.75H9.75V5.25H5.25V9.75ZM3.75 5.24971C3.75 4.42144 4.41557 3.75 5.24971 3.75H9.75029C10.5786 3.75 11.25 4.41557 11.25 5.24971V9.75029C11.25 10.5786 10.5844 11.25 9.75029 11.25H5.24971C4.42144 11.25 3.75 10.5844 3.75 9.75029V5.24971ZM5.25 18.75H9.75V14.25H5.25V18.75ZM3.75 14.2497C3.75 13.4214 4.41557 12.75 5.24971 12.75H9.75029C10.5786 12.75 11.25 13.4156 11.25 14.2497V18.7503C11.25 19.5786 10.5844 20.25 9.75029 20.25H5.24971C4.42144 20.25 3.75 19.5844 3.75 18.7503V14.2497ZM14.25 18.75H18.75V14.25H14.25V18.75ZM12.75 14.2497C12.75 13.4214 13.4156 12.75 14.2497 12.75H18.7503C19.5786 12.75 20.25 13.4156 20.25 14.2497V18.7503C20.25 19.5786 19.5844 20.25 18.7503 20.25H14.2497C13.4214 20.25 12.75 19.5844 12.75 18.7503V14.2497ZM14.25 9.75H18.75V5.25H14.25V9.75ZM12.75 5.24971C12.75 4.42144 13.4156 3.75 14.2497 3.75H18.7503C19.5786 3.75 20.25 4.41557 20.25 5.24971V9.75029C20.25 10.5786 19.5844 11.25 18.7503 11.25H14.2497C13.4214 11.25 12.75 10.5844 12.75 9.75029V5.24971Z"
+                        fill="#BABAC0"
+                      ></path>
+                    </svg>
 
-                <Link href={"/"} className=" ">
-                  {c.title}
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-        <div>
-          {products?.products?.map((c) => {
-            return (
-              <div
-                key={c.id}
-                className="flex  items-center hover:bg-slate-300 transition-all 0.7s"
-              >
-                <Image width={50} height={50} src={c.image} alt={c.name} />
-                <span className="font-bold">{c.name}</span>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+                    <Link href={"/"} className=" ">
+                      {c.title}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              {products?.products?.map((c) => {
+                return (
+                  <div
+                    key={c.id}
+                    className="flex  items-center hover:bg-slate-300 transition-all 0.7s"
+                  >
+                    <Image width={50} height={50} src={c.image} alt={c.name} />
+                    <span className="font-bold">{c.name}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
